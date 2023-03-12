34036187cbfa391c6568cbcc0d13eb6a95b2d36c
Abrar Mohammed Shaikh <75785223+Abrarsh22@users.noreply.github.com>
2023-03-12 22:50:58 +0530

    Update productDB.js

--- productDB.js
+++ productDB.js
@@ -6,7 +6,7 @@ const ProductsJson = require("./data.json");
 //uri="mongodb+srv://Abrarsh2209:yfh5JEt7lTb53ZxM@cluster0.2kgbnxq.mongodb.net/AbrarAPI?retryWrites=true&w=majority";
 const start = async()=>{
     try{
-        await connectDB("mongodb+srv://Abrarsh2209:yfh5JEt7lTb53ZxM@cluster0.2kgbnxq.mongodb.net/AbrarAPI?retryWrites=true&w=majority");
+        await connectDB("mongodb://mongo:pCaTfWJVx1BePOCgVjBo@containers-us-west-183.railway.app:5492");
         await Product.deleteMany();
         await Product.create(ProductsJson);
         console.log("success");
